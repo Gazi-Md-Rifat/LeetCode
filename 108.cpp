529edccec838de497f3a7488b5a57deebab0bf21
Gazi Md. Rifat <133127411+Gazi-Md-Rifat@users.noreply.github.com>
2023-09-28 21:54:08 +0600

    Add files via upload

---
+++ 108.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *buildBST(vector<int> nums, int start, int end){ // timr complexity O(N);
+        if (start > end){
+            return nullptr;
+        }
+        int mid = (start + end) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = buildBST(nums, start, mid - 1);
+        root->right = buildBST(nums, mid + 1, end);
+        return root;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        TreeNode *root = buildBST(nums, 0, nums.size() - 1);  //  staff function.
+        return root; 
+    }
+};
