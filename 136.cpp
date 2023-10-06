cdda0aa428ae407efbfcaaadc72c0ebf25b4725b
Gazi Md. Rifat <133127411+Gazi-Md-Rifat@users.noreply.github.com>
2023-10-06 22:42:06 +0600

    Add files via upload

--- 136.cpp
+++ 136.cpp
@@ -1,36 +1,12 @@
-// class Solution {
-// public:
-//     int singleNumber(vector<int>& nums) {
-//         int n = sizeof(nums) / sizeof(nums[0]);
-//         if (n == 1)
-//         {
-//             return nums[0];
-//         }
-//         for (int i = 0; i < n; i++)   ........this algorith is right but not fullfiled question condition...condition is constant complexity and space..
-//         {
-//             for (int j = i + 1; j < n; j++)
-//             {
-//                 if (nums[i] == nums[j])
-//                 {
-//                     break;
-//                 }
-//                 if (j == n - 1)
-//                 {
-//                     return nums[i];
-//                 }
-//             }
-//         }
-//         return 0; // we  never can arrive this possition.becauge we returned before came this statement...
-//     }
-// };
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) { 
-        int res = 0;
-        for(int x: nums) { // this is XOR concept....because condition is constant complexity and space...thats why i use XOR concept..
-            res ^= x;
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i += 2){ // increase idx each time 2X1.because we check each time 2 idx..i and (i - 1).thats why i += 2;
+            if (nums[i] != nums[i - 1]){
+                return nums[i - 1];
+            }
         }
-        return res;
+        return nums[nums.size() - 1];
     }
 };
