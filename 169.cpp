8249fa7736acff783a3d337b8bc810fccfc57ca6
Gazi Md. Rifat <133127411+Gazi-Md-Rifat@users.noreply.github.com>
2024-02-12 20:57:58 +0600

    Add files via upload

--- 169.cpp
+++ 169.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        return nums[nums.size()/2]; // question include thata majority element exist more than n/2.. thats mean if i sort containr it can apprear till more than first half of the container or it can appear before half to end.becauge its exist mote than n/2 times..
+        return nums[nums.size()/2];
     }
 };
---
+++ 326.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 1){
+            return true;
+        }
+        for (int i = 1; i <= n; i++){
+            if (pow(3, i) == n){
+                return true;
+            }
+            if (pow(3, i) >= n){
+                break;
+            }
+        }
+        return false;
+    }
+};
